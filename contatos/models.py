74b561bec4208111283135d290fabc61ff8a2eec
danielfitipaldi <dwfitipaldi@gmail.com>
2020-09-25 02:47:01 -0300

    projeto e models criados. Conexão com sqlite e configuração do Admin

---
+++ contatos/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Categoria(models.Model):
+    nome = models.CharField(
+        max_length=255
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+
+    nome = models.CharField(
+        max_length=255
+    )
+    sobrenome = models.CharField(
+        blank=True,
+        max_length=255
+    )
+    telefone = models.CharField(
+        max_length=255
+    )
+    email = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    data_criacao = models.DateTimeField(
+        default=timezone.now
+    )
+    descricao = models.TextField(
+        blank=True
+    )
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.DO_NOTHING
+    )
+
+    def __srt__(self):
+        return self.sobrenome
+
